2e6040a1fbc1b6b56a008870b4a55a36417b5107
Shourjyo <77356417+Sh-2402@users.noreply.github.com>
2026-07-23 23:46:55 +0530

    deletion

--- scripts/data_cleaning.py
+++ scripts/data_cleaning.py
@@ -4,16 +4,7 @@ import pandas as pd
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
 
-conn = mysql.connector.connect(
-    host=os.getenv("DB_HOST"),
-    user=os.getenv("DB_USER"),
-    password=os.getenv("DB_PASSWORD"),
-    database=os.getenv("DB_NAME")
-)
-
-conn.close()
 
 df = pd.read_csv("data/order_updated.csv")
 
